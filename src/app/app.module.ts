2bb6ecae3bc10c2a4f67c26c7acc390bceb31cb8
gf <gianfranco.festi@gmail.com>
2021-01-09 18:03:58 +0100

    slice index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,FormsModule,MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
